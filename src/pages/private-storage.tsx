bc370435b6a8d95510dfb283e25a8fe07325289d
shahadpichen <pichenshahad@gmail.com>
2024-10-08 11:06:42 +0530

    changes in ui

--- src/pages/private-storage.tsx
+++ src/pages/private-storage.tsx
@@ -15,22 +15,20 @@ function PrivateStorage() {
   return (
     <section className="bg-[#FAF9F6] w-full h-screen">
       <Toaster />
-      <header className="flex z-10 justify-between pt-5 md:pb-0 pb-3 items-center gap-4 px-4 lg:h-[60px] lg:px-10">
+      <header className="h-[10vh] flex z-10 justify-between pt-5 md:pb-0 pb-3 items-center gap-4 px-4 lg:h-[60px] lg:px-10">
         <h1 className="text-2xl font-bold">ZeroDrive</h1>
         <div className="flex gap-2">
           <GoogleAuth onAuthChange={handleAuthChange} />
         </div>
       </header>
 
-      <div className="flex w-full">
-        <main className="flex flex-1 flex-col gap-4 p-4 pt-2 lg:gap-6 lg:p-6 lg:pt-3">
-          {isAuthenticated ? (
-            <FileList />
-          ) : (
-            <p>Please log in to see your files.</p>
-          )}
-        </main>
-      </div>
+      <main className="flex flex-1 flex-col gap-4 pr-4 pl-4 pt-2 lg:gap-6 lg:pr-6 lg:pl-6 lg:pt-3">
+        {isAuthenticated ? (
+          <FileList />
+        ) : (
+          <p>Please log in to see your files.</p>
+        )}
+      </main>
     </section>
   );
 }
